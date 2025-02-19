7c601169368510ef0a01bc051f96ccbbc5126cf7
Divyanshi Srivastava <divyanshisrivastava@Mac.attlocal.net>
2025-02-19 11:32:36 -0800

    python

--- problem2.py
+++ problem2.py
@@ -1,28 +1,34 @@
 '''
-// Time Complexity : O(n)
-// Space Complexity : O(1)
+// Time Complexity : O(n*n)
+// Space Complexity : O(n)
 // Did this code successfully run on Leetcode : Yes
 // Three line explanation of solution in plain english : same implementation as in class
 
 // Your code here along with comments explaining your approach
 '''
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        p0 = 0
-        p2 = len(nums) - 1
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        if not nums or len(nums) < 3:
+            return result
+        n = len(nums)
+        nums.sort()
+        for i in range(0, n - 2):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l, r = i + 1, n - 1
 
-        cur = 0
-        while cur <= p2:
-            if nums[cur] == 0:
-                nums[p0], nums[cur] = nums[cur], nums[p0]
-                p0 += 1
-                cur += 1
-            elif nums[cur] == 2:
-                nums[p2], nums[cur] = nums[cur], nums[p2]
-                p2 -= 1
-            else:
-                cur += 1
-        
+            while l < r:
+                cur = nums[i] + nums[l] + nums[r]
+                if cur > 0:
+                    r -= 1
+                elif cur < 0:
+                    l += 1
+                else:
+                    result.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while nums[l - 1] == nums[l] and l < r:
+                        l += 1
+        return result
